80c8e73c3346e037daa4c5732ce2fb52758fe977
Alex Chiang <alex@chizang.net>
2013-04-12 23:09:16 -0700

    settings/staging: small cleanups
    Need to set ALLOWED_HOSTS, else we get a 500 error.
    
    And fix the missing comma.

--- raven/settings/staging.py
+++ raven/settings/staging.py
@@ -1,12 +1,14 @@
 from .base import *
 
+ALLOWED_HOSTS = ['.herokuapp.com']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'd97cr2v8mtahbt',
         'USER': 'emsbqjbpbijsgv',
         'PASSWORD': '4V-Dx8cQ_EZs0FZHE2JzYbH4sc',
-        'HOST': 'ec2-23-21-161-255.compute-1.amazonaws.com'
+        'HOST': 'ec2-23-21-161-255.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }

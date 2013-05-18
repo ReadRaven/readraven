c4b42569f54592fd0a3eb2e9d81876c0a0a2e3dd
Paul Hummer <paul@eventuallyanyway.com>
2013-05-17 21:47:14 -0600

    Add in the feed specific view.

--- static/js/reader.js
+++ static/js/reader.js
@@ -4,23 +4,46 @@ window.APP = window.APP || {Routers: {}, Collections: {}, Models: {}, Views: {}}
 
 APP.Routers.ReaderRouter = Backbone.Router.extend({
     routes: {
-        '/': 'index'
+        'feed/:id': 'feed',
+        '*default': 'reader'
     },
-    initialize: function(options) {
-        this.feeds = new APP.Collections.Feeds();
-        this.items = new APP.Collections.FeedItems();
-        this.feeds.fetch().then(_.bind(function(args) {
-            this.items.fetch().then(_.bind(function(args) {
-                this.index();
+    initializer: function(config) {
+        this.feeds = this.feeds || new APP.Collections.Feeds();
+        // TODO: fetch all feeds if they haven't been previously fetched.
+    },
+    reader: function() {
+        var feeds = this.feeds || new APP.Collections.Feeds();
+        var items = new APP.Collections.FeedItems();
+        feeds.fetch().then(_.bind(function(args) {
+            if (this.feeds == undefined) {
+                this.feeds = feeds;
+            }
+            items.fetch().then(_.bind(function(args) {
+                this.currentView = new APP.Views.Reader({
+                    feeds: feeds,
+                    items: items
+                });
+                $('#main').html(this.currentView.render().el);
             }, this));
         }, this));
     },
-    index: function() {
-        this.currentView = new APP.Views.Reader({
-            feeds: this.feeds,
-            items: this.items
-        });
-        $('#main').html(this.currentView.render().el);
+    feed: function(id) {
+        var feeds = this.feeds || new APP.Collections.Feeds();
+        feeds.fetch().then(_.bind(function(args) {
+            if (this.feeds == undefined) {
+                this.feeds = feeds;
+            }
+            var feed = feeds.where({id: parseInt(id, 10)})[0];
+            var items = new APP.Collections.FeedItems(feed.get('items'));
+            items.fetch().then(_.bind(function(args) {
+                this.currentView = new APP.Views.Reader({
+                    feed: feed,
+                    feeds: feeds,
+                    items: feed.get('items')
+                });
+                $('#main').html(this.currentView.render().el);
+            }, this));
+        }, this));
     }
 });
 

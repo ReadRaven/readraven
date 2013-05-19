c2b893bdf4211991b7048c6ccfaafdec086b0f68
Paul Hummer <paul@eventuallyanyway.com>
2013-05-19 11:43:26 -0600

    Add the date to a feed item view

--- static/js/reader.js
+++ static/js/reader.js
@@ -2,6 +2,11 @@
 "use strict";
 window.APP = window.APP || {Routers: {}, Collections: {}, Models: {}, Views: {}};
 
+Handlebars.registerHelper('formatDate', function(context, block) {
+    //var f = block.hash.format || "MMM Do, YYYY";
+    return moment(context).fromNow();
+});
+
 APP.Routers.ReaderRouter = Backbone.Router.extend({
     routes: {
         'feed/:id': 'feed',

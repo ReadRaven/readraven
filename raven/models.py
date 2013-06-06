c588f2072383b38307de9c000c9703828de3536c
Alex Chiang <alex@chizang.net>
2013-06-06 12:11:45 -0700

    raven/models/feed: more defensiveness regarding created_parsed
    I hate rss.

--- raven/models.py
+++ raven/models.py
@@ -222,6 +222,7 @@ class Feed(models.Model):
                     # In this case, there's a "date", but it's unparseable,
                     # i.e. it's something silly like "No date found",
                     # which isn't a date.
+                    hack_extra_sucky = True
                     item.published = datetime.utcnow()
                 else:
                     # This warns about naive timestamps when timezone
@@ -230,12 +231,24 @@ class Feed(models.Model):
                         calendar.timegm(entry.published_parsed))
             except AttributeError:
                 try:
-                    item.published = datetime.utcfromtimestamp(
-                        calendar.timegm(entry.updated_parsed))
+                    if entry.updated_parsed is None:
+                        hack_extra_sucky = True
+                        item.published = datetime.utcnow()
+                        logger.warn('%s: updated_parsed broken: %s' %
+                                    (self.pk, self.link))
+                    else:
+                        item.published = datetime.utcfromtimestamp(
+                            calendar.timegm(entry.updated_parsed))
                 except AttributeError:
                     try:
-                        item.published = datetime.utcfromtimestamp(
-                            calendar.timegm(entry.created_parsed))
+                        if entry.created_parsed is None:
+                            hack_extra_sucky = True
+                            item.published = datetime.utcnow()
+                            logger.warn('%s: created_parsed broken: %s' %
+                                        (self.pk, self.link))
+                        else:
+                            item.published = datetime.utcfromtimestamp(
+                                calendar.timegm(entry.created_parsed))
                     except AttributeError:
                         hack_extra_sucky = True
                         item.published = datetime.utcnow()
